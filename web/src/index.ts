e76d7f345f1186c98c3fca0d6bc48403430f1469
jackieh1288 <jackie.yenchieh.huang@gmail.com>
2023-06-20 21:59:13 +0800

    user form and user collection view with functions
    marionette, backbone-alike framework design

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,14 +1,28 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
+import { UserList } from './views/UserList';
 
-const user = User.buildUser({ name: 'Name', age: 20 });
+const user = User.buildUser({ name: 'customName', age: 20 });
+const collection = User.buildUserCollection();
 
 const root = document.getElementById('root');
 
 if (root) {
-  const userForm = new UserForm(root, user);
+  const userEdit = new UserEdit(root, user);
 
-  userForm.render();
+  userEdit.render();
+
+  console.log(userEdit);
 } else {
   throw new Error('Root element not found');
 }
+
+collection.on('change', () => {
+  const list = document.getElementById('list');
+
+  if (list) {
+    new UserList(list, collection).render();
+  }
+});
+
+collection.fetch();
